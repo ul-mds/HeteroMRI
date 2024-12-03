051af18493ed1e02798cd41f4d7f64e5b25a9dcc
Masoud Abedi <64810541+MasoudAbedi@users.noreply.github.com>
2024-12-03 10:35:36 +0100

    Add DICE on scan_input_MRI.py
    Add DICE and update code.

--- scan_input_MRI.py
+++ scan_input_MRI.py
@@ -8,11 +8,13 @@ import pandas as pd
 import csv
 import os
 import nibabel as nib
+import numpy as np
 import math
 csvdata=[]
 csvadata_file_name="MRIs_List.csv"
 address_data_file_name="datasets_path.csv"
 output_file="All_MRIs_List_paths_temp.csv"
+template_dice = "./template_dice.nii.gz"
 address_data=[]
 
 # Remove the output file if it already exists
@@ -24,7 +26,29 @@ with open(address_data_file_name, newline='') as f:
     reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
     for row in reader:
         address_data.append(row)
+#DICE
+def load_nifti_file(file_path):
+    nifti_img = nib.load(file_path)
+    img = nifti_img.get_fdata()
+    img[img < 0.50] = 0
+    img[img >= 0.50] = 1
 
+    return img
+
+segmentation1 = load_nifti_file(template_dice)
+seg1 = segmentation1.flatten()
+sum_seg1 = np.sum(seg1)
+
+def dice_coefficient(segmentation2):
+    seg2 = segmentation2.flatten()
+
+    intersection = np.inner(seg1,seg2)
+    sum_seg2 = np.sum(seg2)
+    #dice1 = (2 * intersection ) / (sum_seg1 + sum_seg2 )
+    #dice1 =(2 * intersection) / (np.linalg.norm(seg1)**2 + np.linalg.norm(seg2)**2)
+    dice =(2 * intersection) / (np.inner(seg1, seg1) + (np.inner(seg2,seg2)))
+
+    return dice
 all_MRI_dataf=pd.read_csv(csvadata_file_name, sep=',')
 all_MRI=all_MRI_dataf.values.tolist()
 headers=all_MRI_dataf.columns.values.tolist()
@@ -46,19 +70,22 @@ for index in range(len(all_MRI)):
                 for x in os.listdir(mri_file)
             ]
             Not_load=True
+            dice_valuse = []
             for file_path in list_MRI:
-                if file_path[len(file_path)-8]==str(int(all_MRI[index][20])):
-                    try:
-                        img = nib.load(file_path)
-                        all_MRI[index][19]=mri_file
-                        all_MRI[index][21]=file_path
-                    except:
-                        print("Bad file descriptor(warning):"+file_path)
-                    Not_load=False
+                try:
+                    img = load_nifti_file(file_path)  # nib.load(file_path)
+                    dice_valuse.append(dice_coefficient(img))
+                except:
+                    print("Bad file descriptor(warning):" + file_path)
+                    Not_load = False
                     break
-            if Not_load:
+            if not Not_load:
                 print("Cant open file! "+mri_file)
                 Eroor_flag=True
+            else:
+                select_index = dice_valuse.index(max(dice_valuse))
+                all_MRI[index][19] = mri_file
+                all_MRI[index][21] = list_MRI[select_index]
 
             found=False
             break
